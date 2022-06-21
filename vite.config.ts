b48ec4549670df036a2fc23c3b9750bd943cdffc
Laurent Toussaint <laurent@bybr.com>
2022-06-21 16:58:30 +0200

    build: upgrade starknet dep
    - it removes a buggy dependency which forced us to tweak the builder

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import * as path from "path";
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
-import inject from "@rollup/plugin-inject";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,31 +10,6 @@ export default defineConfig({
   resolve: {
     alias: {
       src: path.resolve(__dirname, "./src"),
-    },
-  },
-  optimizeDeps: {
-    // include: ["@starknet-react/core"],
-    esbuildOptions: {
-      // Node.js global to browser globalThis
-      define: {
-        global: "globalThis",
-      },
-      // Enable esbuild polyfill plugins
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true,
-        }),
-      ],
-    },
-  },
-  build: {
-    rollupOptions: {
-      plugins: [
-        inject({
-          // include: ["node_modules/@ledgerhq/**"],
-          modules: { Buffer: ["buffer", "Buffer"] },
-        }),
-      ],
     },
   },
 });

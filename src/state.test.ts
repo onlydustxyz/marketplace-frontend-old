0b3579c66066328af7896bfa1fba892e9016bd82
Stanislas Bernard <bernardstanislas@gmail.com>
2022-07-05 16:31:29 +0200

    test: test single contribution fetching

--- src/state.test.ts
+++ src/state.test.ts
@@ -1,13 +1,17 @@
 import { RecoilRoot, useRecoilValue } from "recoil";
-import { spy } from "sinon";
+import { spy, restore } from "sinon";
 import { renderHook } from "@testing-library/react-hooks";
-import { contributionsQuery } from "./state";
+import { contributionQuery, contributionsQuery } from "./state";
 import { repository } from "./model/contributions/repository";
 import { expect } from "chai";
 
 describe("The recoil state", () => {
+  afterEach(() => {
+    restore();
+  });
+
   describe("when querying contributions", () => {
-    it("uses the repository", async () => {
+    it("uses the repository list function", async () => {
       const listSpy = spy(repository, "list");
 
       const { result, waitForValueToChange } = renderHook(() => useRecoilValue(contributionsQuery), {
@@ -20,6 +24,22 @@ describe("The recoil state", () => {
       await waitForValueToChange(() => result.current);
 
       expect(result.current).to.have.length(1);
+    });
+  });
+
+  describe("when querying a single contribution", () => {
+    it("uses the contributions cache", async () => {
+      // TODO: make this test not depend on the previous one by clearing the recoil cache
+      const listSpy = spy(repository, "list");
+
+      const existingId = "1";
+      const { result } = renderHook(() => useRecoilValue(contributionQuery(existingId)), {
+        wrapper: RecoilRoot,
+      });
+
+      // One can argue that this test also checks the cache ¯\_(ツ)_/¯
+      expect(listSpy).not.to.have.been.called;
+      expect(result.current?.id).to.equal(existingId);
     });
   });
 });

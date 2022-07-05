c8e85927745fb61b3d15ff5096704c1e82f5d2c5
Stanislas Bernard <bernardstanislas@gmail.com>
2022-07-05 16:00:48 +0200

    refactor: rename hook files to kebab case

--- src/hooks/user-information.ts
+++ src/hooks/user-information.ts
@@ -5,7 +5,7 @@ import badgeRegistryAbi from "src/abis/badgeRegistry.json";
 import config from "src/config";
 import { Abi } from "starknet";
 
-export default function useGetUserInformation(account: string | undefined) {
+export default function useUserInformation(account: string | undefined) {
   const { contract } = useContract({ abi: badgeRegistryAbi as Abi, address: config.REGISTRY_CONTRACT_ADDRESS });
   const { data, loading } = useStarknetCall({
     contract,

54273b07cd31231e0ec265ac30a748a240452813
Laurent Toussaint <laurent@bybr.com>
2022-07-06 10:04:34 +0200

    refactor: updates from review

--- src/App/Layout.tsx
+++ src/App/Layout.tsx
@@ -1,15 +1,14 @@
-import { ReactNode } from "react";
+import React from "react";
+
 import Header from "src/components/Header";
 
-interface LayoutProps {
-  children: ReactNode;
-}
-
-export default function Layout({ children }: LayoutProps) {
+const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
     <div className="max-w-screen-lg w-full mx-auto">
       <Header />
       <div className="flex flex-col items-center">{children}</div>
     </div>
   );
-}
+};
+
+export default Layout;

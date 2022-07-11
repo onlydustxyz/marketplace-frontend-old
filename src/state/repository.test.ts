92fdc638c928057aba3236c1fcf59882e0e111a2
Laurent Toussaint <laurent@bybr.com>
2022-07-12 00:14:42 +0200

    build: replace mocha by vitest

--- src/state/repository.test.ts
+++ src/state/repository.test.ts
@@ -1,25 +1,24 @@
+import { afterEach, describe, it, expect, vi } from "vitest";
 import { RecoilRoot, useRecoilValue } from "recoil";
-import { spy, restore } from "sinon";
 import { renderHook } from "@testing-library/react-hooks";
 import { contributionQuery, contributionsQuery } from "./repository";
 import { repository } from "src/model/contributions/repository";
-import { expect } from "chai";
 
 describe("The recoil state", () => {
   afterEach(() => {
-    restore();
+    vi.resetAllMocks();
   });
 
   describe("when querying contributions", () => {
     it("uses the repository list function", async () => {
-      const listSpy = spy(repository, "list");
+      const listSpy = vi.spyOn(repository, "list");
 
       const { result, waitForValueToChange } = renderHook(() => useRecoilValue(contributionsQuery), {
         wrapper: RecoilRoot,
       });
 
       expect(result.current).to.be.undefined;
-      expect(listSpy).to.have.been.called;
+      expect(listSpy).toHaveBeenCalled();
 
       await waitForValueToChange(() => result.current);
 
@@ -30,7 +29,7 @@ describe("The recoil state", () => {
   describe("when querying a single contribution", () => {
     it("uses the contributions cache", async () => {
       // TODO: make this test not depend on the previous one by clearing the recoil cache
-      const listSpy = spy(repository, "list");
+      const listSpy = vi.spyOn(repository, "list");
 
       const existingId = "1";
       const { result } = renderHook(() => useRecoilValue(contributionQuery(existingId)), {
@@ -38,7 +37,7 @@ describe("The recoil state", () => {
       });
 
       // One can argue that this test also checks the cache ¯\_(ツ)_/¯
-      expect(listSpy).not.to.have.been.called;
+      expect(listSpy).not.toHaveBeenCalled();
       expect(result.current?.id).to.equal(existingId);
     });
   });

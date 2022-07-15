c083062002306971540913eb2163ed3b1aa29ca6
Laurent Toussaint <laurent@bybr.com>
2022-07-15 03:05:07 +0200

    fix: ui

--- src/components/ContributionList/index.tsx
+++ src/components/ContributionList/index.tsx
@@ -13,7 +13,7 @@ const ContributionList: FC<Props> = ({ className, contributions }) => {
     <ul
       className={classNames(
         className,
-        "w-full grid grid-cols-[repeat(auto-fit,_minmax(360px,_1fr))] gap-x-[2%] gap-y-4 justify-between "
+        "w-full grid grid-cols-[repeat(auto-fill,_minmax(360px,1fr))] gap-x-[2%] gap-y-4 justify-between "
       )}
     >
       {contributions.map(contribution => (

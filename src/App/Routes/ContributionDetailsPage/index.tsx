9d1c6e00c64c25ea7ac12442e218268f6d753c85
Laurent Toussaint <laurent@bybr.com>
2022-07-18 16:25:53 +0200

    feat: send contribution applicants to typeform

--- src/App/Routes/ContributionDetailsPage/index.tsx
+++ src/App/Routes/ContributionDetailsPage/index.tsx
@@ -1,17 +1,26 @@
 import { useParams } from "react-router-dom";
 import { useRecoilValue, useSetRecoilState } from "recoil";
 import ContributionDetailsPage from "./View";
-import { contributionQuery, displayRegisterModalAtom, isGithubRegisteredSelector } from "src/state";
+import {
+  accountAtom,
+  contributionQuery,
+  displayRegisterModalAtom,
+  isGithubRegisteredSelector,
+  userContributorIdSelector,
+  userGithubHandleSelector,
+} from "src/state";
 import { FC, useCallback } from "react";
 
 type PageParams = {
   contributionId: string;
 };
-
 const ContributionDetailsPageContainer: FC = () => {
   const { contributionId } = useParams<PageParams>();
   const contribution = useRecoilValue(contributionQuery(contributionId));
+  const contributorId = useRecoilValue(userContributorIdSelector);
+  const account = useRecoilValue(accountAtom);
   const isGithubRegistered = useRecoilValue(isGithubRegisteredSelector);
+  const userGithubHandle = useRecoilValue(userGithubHandleSelector);
   const setDisplayRegisterModal = useSetRecoilState(displayRegisterModalAtom);
 
   const apply = useCallback(() => {
@@ -20,8 +29,9 @@ const ContributionDetailsPageContainer: FC = () => {
       return;
     }
 
-    // @TODO = implement contribution application
-    console.log("Apply to contribution", contributionId);
+    const applyUrl = `https://1q4jfbhi7he.typeform.com/to/glbmGLMH#wallet=${account?.address}&github=${userGithubHandle}&githubissue=${contribution?.github_link}&contributorid=${contributorId}`;
+
+    window.open(applyUrl, "_blank");
   }, [contributionId, isGithubRegistered]);
 
   return <ContributionDetailsPage contribution={contribution} apply={apply} />;

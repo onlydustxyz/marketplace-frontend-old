b89abcc032b1925479775b76cf455f5914aaf711
Laurent Toussaint <laurent@bybr.com>
2022-07-25 08:44:29 +0200

    feat: implement global error boundary

--- src/App/Routes/Layout/index.tsx
+++ src/App/Routes/Layout/index.tsx
@@ -4,9 +4,9 @@ import Header from "./Header";
 
 const Layout: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className="mx-auto">
+    <div className="mx-auto flex flex-col min-h-screen">
       <Header />
-      <div className="flex flex-col items-center">{children}</div>
+      <div className="flex-grow flex flex-col items-center">{children}</div>
     </div>
   );
 };
--- src/App/Routes/index.tsx
+++ src/App/Routes/index.tsx
@@ -1,6 +1,8 @@
 import { lazy, Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Loader from "src/icons/Loader";
+import ErrorFallbackRouterAware from "./ErrorFallbackRouterAware";
 import Layout from "./Layout";
 import Modals from "./Modals";
 
@@ -15,48 +17,50 @@ export default function AppRoutes() {
     <BrowserRouter>
       <Layout>
         <Modals>
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <Suspense fallback={<Loader className="animate-spin mt-[30vh]" size={62} />}>
-                  <HomePage />
-                </Suspense>
-              }
-            />
-            <Route
-              path="/projects"
-              element={
-                <Suspense fallback={<Loader className="animate-spin mt-[30vh]" size={62} />}>
-                  <ProjectsPage />
-                </Suspense>
-              }
-            />
-            <Route
-              path="/contributions/:contributionId"
-              element={
-                <Suspense fallback={<Loader className="animate-spin mt-[30vh]" size={62} />}>
-                  <ContributionDetailsPage />
-                </Suspense>
-              }
-            />
-            <Route
-              path="/projects/:projectId"
-              element={
-                <Suspense fallback={<Loader className="animate-spin mt-[30vh]" size={62} />}>
-                  <ProjectDetailsPage />
-                </Suspense>
-              }
-            />
-            <Route
-              path="/my-contributions"
-              element={
-                <Suspense fallback={<Loader className="animate-spin mt-[30vh]" size={62} />}>
-                  <MyContributionsPage />
-                </Suspense>
-              }
-            />
-          </Routes>
+          <ErrorBoundary FallbackComponent={ErrorFallbackRouterAware}>
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <Suspense fallback={<Loader className="animate-spin mt-[30vh]" size={62} />}>
+                    <HomePage />
+                  </Suspense>
+                }
+              />
+              <Route
+                path="/projects"
+                element={
+                  <Suspense fallback={<Loader className="animate-spin mt-[30vh]" size={62} />}>
+                    <ProjectsPage />
+                  </Suspense>
+                }
+              />
+              <Route
+                path="/contributions/:contributionId"
+                element={
+                  <Suspense fallback={<Loader className="animate-spin mt-[30vh]" size={62} />}>
+                    <ContributionDetailsPage />
+                  </Suspense>
+                }
+              />
+              <Route
+                path="/projects/:projectId"
+                element={
+                  <Suspense fallback={<Loader className="animate-spin mt-[30vh]" size={62} />}>
+                    <ProjectDetailsPage />
+                  </Suspense>
+                }
+              />
+              <Route
+                path="/my-contributions"
+                element={
+                  <Suspense fallback={<Loader className="animate-spin mt-[30vh]" size={62} />}>
+                    <MyContributionsPage />
+                  </Suspense>
+                }
+              />
+            </Routes>
+          </ErrorBoundary>
         </Modals>
       </Layout>
     </BrowserRouter>

e0f9894ce6695539b4f6528520026e8fb4393a9e
Laurent Toussaint <laurent@bybr.com>
2022-08-02 21:08:04 +0200

    refactor: update Github workflows

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,37 @@
 import * as path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import type { ViteSentryPluginOptions } from "vite-plugin-sentry";
 import viteSentry from "vite-plugin-sentry";
-
-const sentryConfig: ViteSentryPluginOptions = {
-  url: "https://sentry.io",
-  authToken: process.env.MARKETPLACE_SENTRY_AUTH_TOKEN,
-  org: process.env.MARKETPLACE_SENTRY_ORG,
-  project: process.env.MARKETPLACE_SENTRY_PROJECT,
-  release: process.env.MARKETPLACE_SENTRY_RELEASE,
-  deploy: {
-    env: "production",
-  },
-  setCommits: {
-    auto: true,
-  },
-  sourceMaps: {
-    include: ["./dist/assets"],
-    ignore: ["node_modules"],
-    urlPrefix: "~/assets",
-  },
-};
 
 // https://vitejs.dev/config/
 export default defineConfig({
   envPrefix: "MARKETPLACE_",
-  plugins: [react(), viteSentry(sentryConfig)],
+  plugins: [
+    react(),
+    viteSentry({
+      url: "https://sentry.io",
+      authToken: process.env.MARKETPLACE_SENTRY_AUTH_TOKEN,
+      org: process.env.MARKETPLACE_SENTRY_ORG,
+      project: process.env.MARKETPLACE_SENTRY_PROJECT,
+      release: process.env.MARKETPLACE_SENTRY_RELEASE,
+      deploy: {
+        env: "production",
+      },
+      setCommits: {
+        auto: true,
+      },
+      sourceMaps: {
+        include: ["./dist/assets"],
+        ignore: ["node_modules"],
+        urlPrefix: "~/assets",
+      },
+      dryRun:
+        !process.env.MARKETPLACE_SENTRY_AUTH_TOKEN ||
+        !process.env.MARKETPLACE_SENTRY_ORG ||
+        !process.env.MARKETPLACE_SENTRY_PROJECT ||
+        !process.env.MARKETPLACE_SENTRY_RELEASE,
+    }),
+  ],
   resolve: {
     alias: {
       src: path.resolve(__dirname, "./src"),

300bb3ce5c2563d31375a86dd6ae5a640276059a
Laurent Toussaint <laurent@bybr.com>
2022-08-03 14:40:10 +0200

    feat: add beta banner

--- src/App/Routes/Layout/Header/index.tsx
+++ src/App/Routes/Layout/Header/index.tsx
@@ -6,27 +6,34 @@ import ProfileHeader from "./ProfileHeader";
 
 const Header: FC = () => {
   return (
-    <header
-      className="header grid h-[120px] pt-[14px] px-12 items-center"
-      style={{ gridTemplateColumns: "1fr 60px 1fr" }}
-    >
-      <div className="text-light-purple/66">
-        <NavLink to="/contributions" className={({ isActive }) => (isActive ? "text-white" : undefined)}>
-          All contributions
-        </NavLink>
-        <NavLink className={({ isActive }) => (isActive ? "text-white ml-8" : "ml-8")} to="/">
-          All projects
-        </NavLink>
-      </div>
-      <div>
-        <Link to="/">
-          <img src={logo} width="68px" />
-        </Link>
-      </div>
-      <div className="flex flex-row justify-end items-center">
-        <Suspense fallback={"Connection..."}>
-          <ProfileHeader />
-        </Suspense>
+    <header className="header pt-[12px]">
+      <a
+        href="https://discord.com/invite/TXBr4pbmCJ"
+        target="_blank"
+        className="d-block w-full h-[40px] flex justify-center items-center bg-light-purple text-space-blue col-span-3"
+      >
+        We are currently in beta running on the testnet. If you have any problem using it, come and chat on our Discord
+        →
+      </a>
+      <div className="h-[120px] grid items-center mx-12" style={{ gridTemplateColumns: "1fr 60px 1fr" }}>
+        <div className="text-light-purple/66">
+          <NavLink to="/contributions" className={({ isActive }) => (isActive ? "text-white" : undefined)}>
+            All contributions
+          </NavLink>
+          <NavLink className={({ isActive }) => (isActive ? "text-white ml-8" : "ml-8")} to="/">
+            All projects
+          </NavLink>
+        </div>
+        <div>
+          <Link to="/">
+            <img src={logo} width="68px" />
+          </Link>
+        </div>
+        <div className="flex flex-row justify-end items-center">
+          <Suspense fallback={"Connection..."}>
+            <ProfileHeader />
+          </Suspense>
+        </div>
       </div>
     </header>
   );

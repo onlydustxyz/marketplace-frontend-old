7cffedd4abfe1bddbf53aa46491bcd2b4039edc1
Laurent Toussaint <laurent@bybr.com>
2022-08-03 11:17:49 +0200

    fix: fix contributor_id format

--- src/model/applications/fetched-repository.ts
+++ src/model/applications/fetched-repository.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import config from "src/config";
+import { padLeft } from "starknet/dist/utils/encode";
 
 import { ApplicationRepository, CreateParams, HasContributorAppliedToContributionParams } from "./repository";
 
@@ -20,7 +21,7 @@ export class FetchedApplicationRepository implements ApplicationRepository {
     const endpointUrl = new URL(`${config.DATA_API_HOSTNAME}/contributions/${contributionId}/applications`);
 
     const response = await axios.post(endpointUrl.toString(), {
-      contributorId: contributorId.toString(10),
+      contributor_id: padLeft(contributorId.toString(16), 64),
     });
 
     return response.status === 204;

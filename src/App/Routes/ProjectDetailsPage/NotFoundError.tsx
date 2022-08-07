43ab8dff1c0462727086307ecb18c563de6620a7
danilowhk <danilowhk@gmail.com>
2022-08-07 08:12:49 -0300

    chore: contribution/project error pages

---
+++ src/App/Routes/ProjectDetailsPage/NotFoundError.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+import { Link } from "react-router-dom";
+
+const NotFoundError: FC = () => {
+  return (
+    <div className="min-h-screen flex-grow flex flex-col items-center text-center">
+      <div className="mt-40 mb-8 text-4xl text-red-400/70">Oops, it seems this project doesn't exist</div>
+      <Link to="/projects" className="cursor-pointer text-2xl text-light-purple/90 underline">
+        Go back to Projects Page
+      </Link>
+    </div>
+  );
+};
+
+export default NotFoundError;

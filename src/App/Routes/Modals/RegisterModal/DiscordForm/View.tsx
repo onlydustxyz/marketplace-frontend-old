3974f24c4cbc38379173bcdb80fa8b36bfc91d25
Stanislas Bernard <bernardstanislas@gmail.com>
2022-08-18 17:28:15 +0200

    feat: add some basic styling to the discord form

--- src/App/Routes/Modals/RegisterModal/DiscordForm/View.tsx
+++ src/App/Routes/Modals/RegisterModal/DiscordForm/View.tsx
@@ -1,25 +1,32 @@
 import { FC, FormEvent } from "react";
+import Button from "src/components/Button";
 import Loader from "src/icons/Loader";
 
 type Props = {
   loading: boolean;
   error?: Error;
-  onSubmit: (event: FormEvent<HTMLFormElement>) => void;
+  onSubmit: () => void;
   value: string;
   onChange: (value: string) => void;
 };
 
 const DiscordForm: FC<Props> = ({ loading, onChange, onSubmit, value, error }) => {
   if (loading) {
-    return <Loader />;
+    return <Loader className="animate-spin" size={62} />;
   }
   if (error) {
-    return <div>{error.message}</div>;
+    return <div className="text-4xl text-red-400/70">{error.message}</div>;
   }
+  const onFormSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    onSubmit();
+  };
   return (
-    <form onSubmit={onSubmit}>
-      <input value={value} onChange={e => onChange(e.target.value)} />
-      <input type="submit" value="Register" />
+    <form className="flex space-x-8" onSubmit={onFormSubmit}>
+      <input value={value} onChange={e => onChange(e.target.value)} placeholder="Georges_moustaki#1977" />
+      <Button onClick={onSubmit} size="medium">
+        Send
+      </Button>
     </form>
   );
 };
--- src/App/Routes/Modals/RegisterModal/DiscordForm/index.tsx
+++ src/App/Routes/Modals/RegisterModal/DiscordForm/index.tsx
@@ -1,4 +1,4 @@
-import { FC, FormEvent, useCallback, useState } from "react";
+import { FC, useCallback, useState } from "react";
 import DiscordForm from "src/App/Routes/Modals/RegisterModal/DiscordForm/View";
 import useContactInformation from "src/hooks/contact-information";
 
@@ -6,13 +6,9 @@ const DiscordFormContainer: FC = () => {
   const { register, loading, error } = useContactInformation();
   const [discordHandle, setDiscordHandle] = useState("");
 
-  const onSubmit = useCallback(
-    (event: FormEvent<HTMLFormElement>) => {
-      event.preventDefault();
-      register(discordHandle);
-    },
-    [discordHandle]
-  );
+  const onSubmit = useCallback(() => {
+    register(discordHandle);
+  }, [discordHandle]);
 
   return (
     <DiscordForm

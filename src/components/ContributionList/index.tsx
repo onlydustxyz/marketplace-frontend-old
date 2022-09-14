78468ec0dc89fe3d63b049be1e3f47b7e44fd555
Oscar Roche <oscar.w.roche@gmail.com>
2022-09-14 16:55:50 +0200

    feat: memoize / add windowing for contributions grid

--- src/components/ContributionList/index.tsx
+++ src/components/ContributionList/index.tsx
@@ -1,31 +1,36 @@
-import { FC } from "react";
+import { FC, memo } from "react";
 import Contribution from "./Contribution";
 import { Contribution as ContributionType } from "src/state";
 import classNames from "classnames";
 import { Link } from "react-router-dom";
+import { VirtuosoGrid } from "react-virtuoso";
 
 type Props = {
   contributions: ContributionType[];
   className?: string;
 };
 
-const ContributionList: FC<Props> = ({ className, contributions }) => {
+let ContributionList: FC<Props> = ({ className, contributions }) => {
   return (
-    <ul
-      className={classNames(
+    <VirtuosoGrid
+      style={{ height: 400 }}
+      useWindowScroll
+      totalCount={contributions.length}
+      overscan={3}
+      listClassName={classNames(
         "w-full grid grid-col md:grid-cols-[repeat(auto-fill,_minmax(360px,1fr))] gap-x-[3%] gap-y-2 md:gap-y-4 justify-between",
         className
       )}
-    >
-      {contributions.map(contribution => (
-        <li key={contribution.id} className="md:my-2 w-full overflow-hidden">
-          <Link to={`/contributions/${contribution.id}`} className="flex-1">
-            <Contribution contribution={contribution} />
-          </Link>
-        </li>
-      ))}
-    </ul>
+      itemClassName="md:my-2 w-full overflow-hidden"
+      itemContent={index => (
+        <Link to={`/contributions/${contributions[index].id}`} className="flex-1">
+          <Contribution contribution={contributions[index]} />
+        </Link>
+      )}
+    />
   );
 };
+
+ContributionList = memo(ContributionList);
 
 export default ContributionList;

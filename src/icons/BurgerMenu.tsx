077c301ec607b55f8b0bef04f1b1a603374a14a4
Laurent Toussaint <laurent@bybr.com>
2022-09-16 10:34:20 +0200

    fix: fix svg attribute typo

--- src/icons/BurgerMenu.tsx
+++ src/icons/BurgerMenu.tsx
@@ -6,9 +6,9 @@ type Props = {
 export default function BurgerMenu({ className, size = 24 }: Props) {
   return (
     <svg viewBox="0 0 24 24" className={className} height={size} width={size}>
-      <path fill-rule="evenodd" clip-rule="evenodd" d="M23 4.5H1V3.5H23V4.5Z" />
-      <path fill-rule="evenodd" clip-rule="evenodd" d="M23 12.5L1 12.5L1 11.5L23 11.5V12.5Z" />
-      <path fill-rule="evenodd" clip-rule="evenodd" d="M23 20.5H1V19.5H23V20.5Z" />
+      <path fillRule="evenodd" clipRule="evenodd" d="M23 4.5H1V3.5H23V4.5Z" />
+      <path fillRule="evenodd" clipRule="evenodd" d="M23 12.5L1 12.5L1 11.5L23 11.5V12.5Z" />
+      <path fillRule="evenodd" clipRule="evenodd" d="M23 20.5H1V19.5H23V20.5Z" />
     </svg>
   );
 }

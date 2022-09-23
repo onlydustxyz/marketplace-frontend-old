f044241f4f22819fa00066f090bf04487edf327c
Laurent Toussaint <laurent@bybr.com>
2022-09-23 11:28:39 +0200

    feat: remove reference to Application id

--- src/model/applications/in-memory-repository.ts
+++ src/model/applications/in-memory-repository.ts
@@ -1,13 +1,12 @@
 import { ApplicationRepository, ContributionApplicationDto, CreateParams, ListParams } from "./repository";
 
-let indexId = 4;
 export class InMemoryApplicationRepository implements ApplicationRepository {
   private contributionsApplications: Array<ContributionApplicationDto> = [
-    { id: "1", contribution_id: "1", contributor_id: 1 },
-    { id: "2", contribution_id: "2", contributor_id: 2 },
-    { id: "3", contribution_id: "2", contributor_id: 38 },
-    { id: "4", contribution_id: "3", contributor_id: 1 },
-    { id: "5", contribution_id: "4", contributor_id: 38 },
+    { contribution_id: "1", contributor_id: 1 },
+    { contribution_id: "2", contributor_id: 2 },
+    { contribution_id: "2", contributor_id: 38 },
+    { contribution_id: "3", contributor_id: 1 },
+    { contribution_id: "4", contributor_id: 38 },
   ];
 
   public async list({ contributorId }: ListParams): Promise<ContributionApplicationDto[]> {
@@ -19,12 +18,9 @@ export class InMemoryApplicationRepository implements ApplicationRepository {
 
   public async create({ contributionId, contributorId }: CreateParams) {
     this.contributionsApplications.push({
-      id: indexId.toString(),
       contribution_id: contributionId,
       contributor_id: contributorId,
     });
-
-    indexId++;
 
     return true;
   }

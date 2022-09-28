02bb347da8ae424bb31abb47d3556438b3eea76b
Laurent Toussaint <laurent@bybr.com>
2022-09-28 14:49:46 +0200

    test: add some tests

--- cypress/e2e/contributions/actions.cy.ts
+++ cypress/e2e/contributions/actions.cy.ts
@@ -1,16 +1,13 @@
 /// <reference types="cypress" />
 
+import { HeadlessWalletFactory, MockAccount, MockProvider } from "../../../tests/lib/starknet-wallet-mock";
+import { HeadlessWallet } from "../../../tests/lib/starknet-wallet-mock/wallet";
+
+const walletFactory = new HeadlessWalletFactory();
+
+let headlessWallet: HeadlessWallet;
+
 describe("Contribution", () => {
-  beforeEach(() => {
-    Cypress.Cookies.debug(true);
-
-    cy.visit("http://localhost:3000/");
-
-    // clear cookies again after visiting to remove
-    // any 3rd party cookies picked up such as cloudflare
-    cy.clearCookies();
-  });
-
   describe("Test actions", () => {
     it("should open register modal when appliying with no connection", () => {
       cy.visit("http://localhost:3000/contributions/1");
@@ -18,6 +15,72 @@ describe("Contribution", () => {
       cy.getByTestId("button-main-action").click();
 
       cy.getByTestId("register-modal").should("exist");
+    });
+
+    it("should open register modal when appliying with an account without contributor", () => {
+      cy.on("window:before:load", window => {
+        headlessWallet = walletFactory.create(window, {
+          id: "headless-test",
+          name: "Headless Test",
+          windowPropertyName: "starknet_headless",
+          accountFactory: (address: string) => {
+            return new MockAccount(address);
+          },
+          provider: new MockProvider(),
+        });
+
+        headlessWallet.autoConnect({ address: "0x55555555" });
+      });
+
+      cy.visit("http://localhost:3000/contributions/1");
+
+      cy.getByTestId("button-main-action").click();
+
+      cy.getByTestId("register-modal").should("exist");
+    });
+
+    it("should open register modal when appliying with an account with no discord account", () => {
+      cy.on("window:before:load", window => {
+        headlessWallet = walletFactory.create(window, {
+          id: "headless-test",
+          name: "Headless Test",
+          windowPropertyName: "starknet_headless",
+          accountFactory: (address: string) => {
+            return new MockAccount(address);
+          },
+          provider: new MockProvider(),
+        });
+
+        headlessWallet.autoConnect({ address: "0x123456789abcdef" });
+      });
+
+      cy.visit("http://localhost:3000/contributions/1");
+
+      cy.getByTestId("button-main-action").click();
+
+      cy.getByTestId("register-modal").should("exist");
+    });
+
+    it.only("should apply", () => {
+      cy.on("window:before:load", window => {
+        headlessWallet = walletFactory.create(window, {
+          id: "headless-test",
+          name: "Headless Test",
+          windowPropertyName: "starknet_headless",
+          accountFactory: (address: string) => {
+            return new MockAccount(address);
+          },
+          provider: new MockProvider(),
+        });
+
+        headlessWallet.autoConnect({ address: "0x123456789" });
+      });
+
+      cy.visit("http://localhost:3000/contributions/1");
+
+      cy.getByTestId("button-main-action").click();
+
+      cy.getByTestId("toast-container").contains("Thank you for your application for Contribution 1,");
     });
   });
 });

aa7bafc20257eb44764db96adfe2252ceac0029c
Laurent Toussaint <laurent@bybr.com>
2022-09-29 22:22:35 +0200

    test: implement transaction manager

--- tests/lib/starknet-wallet-mock/wallet/headless-wallet.ts
+++ tests/lib/starknet-wallet-mock/wallet/headless-wallet.ts
@@ -1,14 +1,23 @@
+import { getSelectorFromName } from "starknet/dist/utils/hash";
+import { TransactionManager } from "../transaction-manager";
 import { WalletEventEmitter } from "../wallet-event-emitter";
 
 interface ConnectData {
   address: string;
 }
 
+interface TransactionInteractionKey {
+  contractAddress: string;
+  method: string;
+}
+
 export class HeadlessWallet {
   #eventEmitter: WalletEventEmitter;
+  #transactionManager: TransactionManager;
 
-  constructor(eventEmitter: WalletEventEmitter) {
+  constructor(eventEmitter: WalletEventEmitter, transactionManager: TransactionManager) {
     this.#eventEmitter = eventEmitter;
+    this.#transactionManager = transactionManager;
   }
 
   connect(data: ConnectData) {
@@ -17,5 +26,17 @@ export class HeadlessWallet {
 
   autoConnect(data: ConnectData) {
     this.#eventEmitter.emit("autoConnect", data);
+  }
+
+  acceptTransaction({ contractAddress, method }: TransactionInteractionKey) {
+    const methodSelector = getSelectorFromName(method);
+    const hash = (parseInt(contractAddress, 16) + parseInt(methodSelector, 16)).toString(16);
+    this.#transactionManager.acceptTransaction(hash);
+  }
+
+  rejectTransaction({ contractAddress, method }: TransactionInteractionKey, reason = "Reject") {
+    const methodSelector = getSelectorFromName(method);
+    const hash = (parseInt(contractAddress, 16) + parseInt(methodSelector, 16)).toString(16);
+    this.#transactionManager.rejectTransaction(hash, reason);
   }
 }

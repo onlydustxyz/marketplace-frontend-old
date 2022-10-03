ea649445bf75b014727fdf4659489cbec8f890c0
Laurent Toussaint <laurent@bybr.com>
2022-10-03 11:51:05 +0200

    feat: automatically refresh contributions

--- src/App/Routes/ProjectDetailsPage/index.tsx
+++ src/App/Routes/ProjectDetailsPage/index.tsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import { useRecoilValue_TRANSITION_SUPPORT_UNSTABLE } from "recoil";
 import ProjectDetailsPage from "./View";
 import { projectContributionsQuery, projectQuery } from "src/state";
+import useRefreshContributions from "src/hooks/refresh-contributions";
 
 type PageParams = {
   projectId: string;
@@ -12,6 +13,8 @@ const ProjectDetailsPageContainer: FC = () => {
   const { projectId } = useParams<PageParams>();
   const project = useRecoilValue_TRANSITION_SUPPORT_UNSTABLE(projectQuery(projectId));
   const projectContribution = useRecoilValue_TRANSITION_SUPPORT_UNSTABLE(projectContributionsQuery(projectId));
+
+  useRefreshContributions();
 
   return <ProjectDetailsPage project={project} contributions={projectContribution} />;
 };

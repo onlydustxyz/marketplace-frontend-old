2e3fa9e43f879762a045d0365809f4e9020c534d
Laurent Toussaint <laurent@bybr.com>
2022-10-06 11:48:43 +0200

    feat: add coverage for e2e tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,9 +2,15 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    // setupNodeEvents(on, config) {
-    //   // implement node event listeners here
-    // },
+    setupNodeEvents(on, config) {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      require("@cypress/code-coverage/task")(on, config);
+      // include any other plugin code...
+
+      // It's IMPORTANT to return the config object
+      // with any changed environment variables
+      return config;
+    },
     video: false,
     screenshotOnRunFailure: false,
   },

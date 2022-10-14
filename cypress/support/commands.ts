e88ff2ad60213ec59bc640f4b9f0c9731ee386b4
Laurent Toussaint <laurent@bybr.com>
2022-10-14 16:24:27 +0200

    test: increase timeout for a specific test

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -5,14 +5,14 @@ export {};
 declare global {
   namespace Cypress {
     interface Chainable {
-      getByTestId(selector: string): Chainable<JQuery<HTMLElement>>;
+      getByTestId(selector: string, options?: Parameters<typeof cy.get>[1]): Chainable<JQuery<HTMLElement>>;
       executeCallback(callback: () => void): Chainable<JQuery<HTMLElement>>;
     }
   }
 }
 
-Cypress.Commands.add("getByTestId", (selector, ...args) => {
-  return cy.get(`[data-testid=${selector}]`, ...args);
+Cypress.Commands.add("getByTestId", (selector, options) => {
+  return cy.get(`[data-testid=${selector}]`, options);
 });
 
 Cypress.Commands.add("executeCallback", (callback: () => void) => {

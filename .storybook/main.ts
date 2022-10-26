66d6b058fbbd1e87983782ef420af115cd4b7972
Oscar Roche <oscarroche@Oscars-MacBook-Pro.local>
2022-10-26 19:09:41 +0200

    feat: add postcss / tailwind support for storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,9 +1,29 @@
 import type { StorybookViteConfig } from "@storybook/builder-vite";
 import path from "path";
+import postcss from "postcss";
+import * as tailwindcss from "../tailwind.config";
 
 const config: StorybookViteConfig = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: postcss,
+          postcssOptions: {
+            plugins: {
+              tailwindcss,
+              autoprefixer: {},
+            },
+          },
+        },
+      },
+    },
+  ],
   framework: "@storybook/react",
   core: {
     builder: "@storybook/builder-vite",

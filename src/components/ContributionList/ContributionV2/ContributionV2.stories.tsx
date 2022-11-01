4e50f7a13b2751d2d3d61ca40c075a2662fa7f96
Oscar Roche <oscarroche@Oscars-MacBook-Pro.local>
2022-11-01 15:18:14 +0000

    refactor: change / destructure contribution props

--- src/components/ContributionList/ContributionV2/ContributionV2.stories.tsx
+++ src/components/ContributionList/ContributionV2/ContributionV2.stories.tsx
@@ -1,7 +1,7 @@
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
-import ContributionV2 from "src/components/ContributionList/ContributionV2";
-import { ContributionStatusEnum, ContributionWithStatus } from "src/state";
+import ContributionV2, { ContributionProps } from "src/components/ContributionList/ContributionV2";
+import { ContributionStatusEnum } from "src/state";
 
 export default {
   title: "ContributionV2",
@@ -10,18 +10,9 @@ export default {
 
 const Template: ComponentStory<typeof ContributionV2> = args => <ContributionV2 {...args} />;
 
-const defaultContribution: ContributionWithStatus = {
+const defaultContribution: ContributionProps = {
   title: "Test: ",
-  id: "id",
-  project_id: "project_id",
   status: ContributionStatusEnum.OPEN,
-  description: "",
-  github_link: "",
-  gate: 0,
-  gateMissingCompletedContributions: 0,
-  metadata: {},
-  available_slot_count: 1,
-  max_slot_count: 1,
   project: {
     title: "title",
     logo: "https://app.onlydust.xyz/assets/onlydust-logo.5f8cc594.png",
@@ -31,24 +22,18 @@ const defaultContribution: ContributionWithStatus = {
 
 export const Open = Template.bind({});
 
-Open.args = {
-  contribution: defaultContribution,
-};
+Open.args = defaultContribution;
 
 export const Closed = Template.bind({});
 
 Closed.args = {
-  contribution: {
-    ...defaultContribution,
-    status: ContributionStatusEnum.CLOSED,
-  },
+  ...defaultContribution,
+  status: ContributionStatusEnum.CLOSED,
 };
 
 export const Assigned = Template.bind({});
 
 Assigned.args = {
-  contribution: {
-    ...defaultContribution,
-    status: ContributionStatusEnum.ASSIGNED,
-  },
+  ...defaultContribution,
+  status: ContributionStatusEnum.ASSIGNED,
 };

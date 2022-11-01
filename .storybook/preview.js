5c6d3b97c1fcf39c34653a80b31244372c89caf8
Oscar Roche <oscarroche@Oscars-MacBook-Pro.local>
2022-11-01 12:13:53 +0100

    fix: add fonts to storybook preview file

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,7 @@
 import "tailwindcss/tailwind.css";
+import "src/assets/css/index.css";
+import "src/assets/fonts/Alfreda/stylesheet.css";
+import "src/assets/fonts/GTWalsheimPro/stylesheet.css";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
